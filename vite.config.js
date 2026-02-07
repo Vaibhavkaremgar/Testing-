cc4041b32d8ebda28b5bdb708b38e4cd6191a645
Sai Vignesh <suramsaivignesh@gmail.com>
2026-02-07 22:23:36 +0530

    updated the post/pokka

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'athletic-elegance-production-ef72.up.railway.app'
+    ]
+  }
 })
